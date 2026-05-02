f8107447f72aa951c57f8f68e987e34dfe667adb
harshtayal <tayalharsh58@gmail.com>
2026-05-02 23:07:28 +0530

    part 33

---
+++ app/src/main/java/com/example/conquer_app/Influencer.java
@@ -0,0 +1,19 @@
+package com.example.conquer_app;
+
+public class Influencer {
+    private String name, handle, location, category, followers;
+
+    public Influencer(String name, String handle, String location, String category, String followers) {
+        this.name = name;
+        this.handle = handle;
+        this.location = location;
+        this.category = category;
+        this.followers = followers;
+    }
+
+    public String getName() { return name; }
+    public String getHandle() { return handle; }
+    public String getLocation() { return location; }
+    public String getCategory() { return category; }
+    public String getFollowers() { return followers; }
+}
