08a8a4fe482fca9b965764446546b13d32c21683
Rakshit Chhabra <252430991+rakshit1902@users.noreply.github.com>
2026-05-04 19:55:45 +0530

    Home Screen Vector assets, Button menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -43,4 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
